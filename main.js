124517632d5566cc191e5f4ad11fdccc346d6f2d
Jonathon Meng <jonathon.meng@domo.com>
2018-10-01 17:54:58 -0600

    Small refactor, finished parsing

--- main.js
+++ main.js
@@ -84,85 +84,177 @@ function callDisplayTopic(topic) {
   }
 }
 
-function displayFilms() {
-  //Figure out how many columns of data will fit on the page
-  const numColumns = Math.floor(window.innerWidth / 260);
-    
-  //Add bootstrap cards for each result item to its repsective column
-  let filmsHtml = {};
-  films.forEach((film, i) => {
-    console.log("film: " + JSON.stringify(film));
-    const colIndex = i % numColumns;
-    if (filmsHtml[colIndex] === undefined) {
-      filmsHtml[colIndex] = '';
-    }
-    filmsHtml[colIndex] += `
-      <div class=" flex-shrink-0 card bg-dark text-warning item-details">
-        <img class="card-image"/>
-        <button class="btn btn-dark text-warning" type="button" data-toggle="collapse" data-target="#film-${i}" aria-expanded="false" aria-controls="film-${i}">${film.title}</button>
-        <div class="collapse card-content" id="film-${i}">
-          <ul>
-            <li><strong>Director: </strong>${film.director}</li>
-            <li><strong>Producer: </strong>${film.producer}</li>
-            <li><strong>Release Date: </strong>${film.release_date}</li>
-          </ul>
-        </div>
-      </div>
-    `;
-  });
 
-  //Wrap each column with flex-column
-  let finalHtml = '';
-  Object.keys(filmsHtml).forEach(key => {
-    finalHtml += `<div class="d-flex flex-column">${filmsHtml[key]}</div>`;
-  });
-  $('#films-container').html(finalHtml);
+
+function displayFilms() {
+  const filmParser = (film) => {
+    return {
+      'Director': film.director,
+      'Producer': film.producer,
+      'Release Date': film.release_date
+    };
+  };
+
+  const getTitle = (film) => {
+    return film.title;
+  };
+
+  displayItem(films, 'films', 'film', getTitle, filmParser);
 }
 
 function displayPeople() {
+  const peopleParser = (person) => {
+    return {
+      'Gender': _.upperFirst(person.gender), 
+      'Eye Color': _.upperFirst(person.eye_color),
+      'Skin Color': _.upperFirst(person.skin_color),
+      'Height': person.height,
+      'Mass': person.mass
+    };
+  };
+
+  const getTitle = (person) => {
+    return person.name;
+  };
+
+  displayItem(people, 'people', 'person', getTitle, peopleParser);
+}
+
+function displayPlanets() {
+  const planetParser = (planet) => {
+    return {
+      'Population': planet.population,
+      'Climate': planet.climate,
+      'Terrain': planet.terrain,
+      'Gravity': planet.gravity,
+      'Diameter': planet.diameter,
+      'Rotation Period': planet.rotation_period,
+      'Orbital Period': planet.orbital_period,
+    };
+  };
+
+  const getTitle = (planet) => {
+    return planet.name;
+  };
+
+  displayItem(planets, 'planets', 'planet', getTitle, planetParser);
+}
+
+function displaySpecies() {
+  const speciesParser = (species) => {
+    return {
+      'Average Height': species.average_height,
+      'Average Lifespan': species.average_lifespan,
+      'Classification': species.classification,
+      'Designation': species.designation,
+      'Eye Colors': species.eye_colors,
+      'Hair Colors': species.hair_colors,
+      'Language': species.language,
+      'Skin Colors': species.skin_colors,
+    };
+  };
+
+  const getTitle = (species) => {
+    return species.name;
+  };
+
+  displayItem(species, 'species', 'species', getTitle, speciesParser);
+}
+
+function displayStarships() {
+  const starshipsParser = (starship) => {
+    return {
+      'MGLT': starship.MGLT,
+      'Cargo Capacity': starship.cargo_capacity,
+      'Consumables': starship.consumables,
+      'Cost in Credits': starship.cost_in_credits,
+      'Crew': starship.crew,
+      'Hyperdrive Rating': starship.hyperdrive_rating,
+      'Length': starship.length,
+      'Manufacturer': starship.manufacturer,
+      'Max Atmosphering Speed': starship.max_atmosphering_speed,
+      'Model': starship.model,
+      'Passengers': starship.passengers,
+      'Starship Class': starship.starship_class,
+    };
+  };
+
+  const getTitle = (starship) => {
+    return starship.name;
+  };
+
+  displayItem(starships, 'starships', 'starship', getTitle, starshipsParser);
+}
+
+function displayVehicles() {
+  const vehiclesParser = (vehicle) => {
+    return {
+      'Cargo Capacity': vehicle.cargo_capacity,
+      'Consumables': vehicle.consumables,
+      'Cost in Credits': vehicle.cost_in_credits,
+      'Crew': vehicle.crew,
+      'Length': vehicle.length,
+      'Manufacturer': vehicle.manufacturer,
+      'Max Atmosphering Speed': vehicle.max_atmosphering_speed,
+      'Model': vehicle.model,
+      'Passengers': vehicle.passengers,
+      'Vehicle Class': vehicle.vehicle_class
+    };
+  };
+
+  const getTitle = (vehicle) => {
+    return vehicle.name;
+  };
+
+  displayItem(vehicles, 'vehicles', 'vehicle', getTitle, vehiclesParser);
+}
+
+function displayItem(itemList, type, singleType, itemTitle, itemParser) {
   //Figure out how many columns of data will fit on the page
   const numColumns = Math.floor(window.innerWidth / 260);
-  
+ 
   //Add bootstrap cards for each result item to its repsective column
-  let peopleHtml = {};
-  people.forEach((person, i) => {
+  let itemsHtml = {};
+  itemList.forEach((item, i) => {
     const colIndex = i % numColumns;
-    if (peopleHtml[colIndex] === undefined) {
-      peopleHtml[colIndex] = '';
+    if (itemsHtml[colIndex] === undefined) {
+      itemsHtml[colIndex] = '';
     }
-    peopleHtml[colIndex] += `
-			<div class=" flex-shrink-0 card bg-dark text-warning item-details">
-				<img class="card-image"/>
-				<button class="btn btn-dark text-warning" type="button" data-toggle="collapse" data-target="#person-${i}" aria-expanded="false" aria-controls="person-${i}">${person.name}</button>
-				<div class="collapse card-content" id="person-${i}">
-					<ul>
-						<li><strong>Gender: </strong>${_.upperFirst(person.gender)}</li>
-						<li><strong>Eye Color: </strong>${_.upperFirst(person.eye_color)}</li>
-						<li><strong>Skin Color: </strong>${_.upperFirst(person.skin_color)}</li>
-						<li><strong>Height: </strong>${person.height}</li>
-						<li><strong>Mass: </strong>${person.mass}</li>
-					</ul>
-				</div>
-			</div>
-		`;
+
+    parsedItem = itemParser(item);
+    itemsHtml[colIndex] += getCard(singleType, i, itemTitle(item), itemParser(item));
   });
 
   //Wrap each column with flex-column
   let finalHtml = '';
-  Object.keys(peopleHtml).forEach(key => {
-    finalHtml += `<div class="d-flex flex-column">${peopleHtml[key]}</div>`;
+  Object.keys(itemsHtml).forEach(key => {
+    finalHtml += `<div class="d-flex flex-column">${itemsHtml[key]}</div>`;
   });
-  $('#people-container').html(finalHtml);
+  $(`#${type}-container`).html(finalHtml);
 }
 
+function getCard(type, itemNum, title, obj) {
+  return `
+    <div class=" flex-shrink-0 card bg-dark text-warning item-details">
+      <img class="card-image"/>
+      <button class="btn btn-dark text-warning" type="button" data-toggle="collapse" data-target="#${type}-${itemNum}" aria-expanded="false" aria-controls="${type}-${itemNum}">${title}</button>
+      <div class="collapse card-content" id="${type}-${itemNum}">
+        <ul>
+          ${getCardList(obj)}
+        </ul>
+      </div>
+    </div>
+  `;
+}
 
-function displayPlanets() {}
-
-function displaySpecies() {}
-
-function displayStarships() {}
-
-function displayVehicles() {}
+// Takes an object a keys (Names) and values
+function getCardList(obj) {
+  let list = '';
+  Object.keys(obj).forEach(key => {
+    list += `<li><strong>${key}: </strong>${obj[key]}</li>\n`;
+  });
+  return list;
+}
 
 function toggleCollapse(topic) {
   const containerId = `#${topic}-container`;
